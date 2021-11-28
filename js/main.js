e887dceba2472d1d2a9d7e86fc7c14e8f738ff47
grey41 <ford@greyhope.uk>
2021-11-28 14:05:34 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() {dataLayer.push(arguments);}
 gtag("js", new Date());
-gtag("config", "G-7WP82B4W69");
+gtag("config", "G-LD4L8FCK6D");
 
 onload = () => {
     if (typeof year != "undefined")
