2a560b05389410db14849eeb6e795a886e064104
JoBase <hello@jobase.org>
2022-07-26 17:06:07 +0100

    Update 2022

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+const mutation = new MutationObserver(() => {
+    if (!document.body) return
+
+    const theme = localStorage.getItem("theme")
+    const dark = matchMedia("(prefers-color-scheme:dark)").matches
+
+    mutation.disconnect()
+    document.body.setAttribute("theme", theme ? theme : dark ? "dark" : "light")
+})
+
+mutation.observe(document.documentElement, {childList: true})
 window.dataLayer = window.dataLayer || []
-function gtag() {dataLayer.push(arguments)}
 
 gtag("js", new Date())
 gtag("config", "G-LD4L8FCK6D", {cookie_flags: "SameSite=None;Secure"})
+
+function gtag() {
+    dataLayer.push(arguments)
+}
 
 function bars(item) {
     const top = item.parentNode
@@ -13,7 +27,24 @@ function bars(item) {
     else top.className = "top"
 }
 
+function path(theme) {
+    return `/assets/images/icons/${theme}.png`
+}
+
+function theme(item) {
+    const theme = document.body.getAttribute("theme")
+    const current = theme == "dark" ? "light" : "dark"
+
+    localStorage.setItem("theme", current)
+    document.body.setAttribute("theme", current)
+
+    item.src = path(current)
+}
+
 onload = () => {
     const year = document.querySelector("span[year]")
+    const icon = document.querySelector(".theme")
+
     if (year) year.textContent = new Date().getFullYear()
+    icon.src = path(document.body.getAttribute("theme"))
 }
