8b87feaf153ecff5d22d60c69bb93c2236709b0f
nathanforduk <info@nathanford.co.uk>
2024-03-26 14:30:11 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,33 +10,19 @@ const mutation = new MutationObserver(() => {
 
 mutation.observe(document.documentElement, {childList: true})
 
-const sliderContainer = []
+const slidersContainer = []
 
 function change(dir, jump = 2, index) {
-    const item = sliderContainer[index]
-    const slider = item.slider
+    // get the slider div element
+    const item = slidersContainer[index]
 
-    const images = slider.children.length
-    const atEnd = (item.idx + dir > Math.floor(images / jump) - 1)
-
-    if (slider.id.length || (item.idx + dir < 0)) return
-    slider.id = 'scroll'
-
-    const tempImage = slider.children[0]
+    // get the width of a template image
+    const tempImage = item.slider.children[0]
     const increment = tempImage.width * jump
 
-    slider.style.setProperty('--transformOffsetGoal', 'translateX(' + item.pos + 'px)')
-
-    if (atEnd) {
-        item.pos = 0
-        item.idx = 0
-    }
-    else {
-        item.pos -= increment * dir
-        item.idx += dir
-    }
-
-    slider.style.setProperty('--transformOffsetStart', 'translateX(' + item.pos + 'px)')
+    // move div in specified direction
+    item.pos -= increment * dir
+    item.slider.style.transform = 'translateX(' + item.pos + 'px)'
 }
 
 function bars(item) {
@@ -70,21 +56,15 @@ onload = () => {
     icon.src = path(document.body.getAttribute("theme"))
 
     const sliders = document.querySelectorAll('.slider')
-    for (let i = 0; i < sliders.length; i ++) {
-        sliders[i].addEventListener('animationend', () => sliders[i].id = '')
-        sliderContainer.push({slider: sliders[i], idx: 0, pos: 0})
-    }
+    for (let i = 0; i < sliders.length; i ++)
+        slidersContainer.push({slider: sliders[i], pos: 0})
 }
 
 onresize = () => {
-    for (let i = 0; i < sliderContainer.length; i ++) {
-        const slider = sliderContainer[i].slider
-        slider.id = 'scroll'
-
-        sliderContainer[i].pos = 0
-        sliderContainer[i].idx = 0
-
-        slider.style.setProperty('--transformOffsetStart', 'translateX(0)')
-        slider.style.setProperty('--transformOffsetGoal', 'translateX(0)')
+    // the sliders are reset so that the images don't get jumbled
+    for (let i = 0; i < slidersContainer.length; i ++) {
+        const item = slidersContainer[i]
+        item.pos = 0
+        item.slider.style.transform = 'translateX(0)'
     }
 }
